c05284cd7d05aac67d5ea04f2efb2384ab8001d5
kamar-1000 <kamaralam366@gmail.com>
2019-08-07 09:44:40 +0530

    some changes

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -127,5 +127,5 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
-EMAIL_HOST_USER='kamaralam.1000@gmail.com'
-EMAIL_HOST_PASSWORD='K@#$904537'
+EMAIL_HOST_USER='kamarproject.1000@gmail.com'
+EMAIL_HOST_PASSWORD='Test@123'
